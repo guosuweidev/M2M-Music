4bd2c0ebe7fcedcf5a5ad6fef20cf27cf4206ffd
gsw <guosuweilw@qq.com>
2021-03-20 16:45:03 +0800

    chore: 完善闪屏页逻辑（2秒自动跳转），添加ViewBinding取代KAE

--- app/src/main/java/com/m2mmusic/android/ui/MainActivity.kt
+++ app/src/main/java/com/m2mmusic/android/ui/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.m2mmusic.android.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.m2mmusic.android.R
+import com.m2mmusic.android.databinding.ActivityMainBinding
+import com.m2mmusic.android.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        binding.mainText.text = "MainActivity"
 
     }
 }
--- app/src/main/java/com/m2mmusic/android/ui/SplashActivity.kt
+++ app/src/main/java/com/m2mmusic/android/ui/SplashActivity.kt
@@ -1,12 +1,32 @@
 package com.m2mmusic.android.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.m2mmusic.android.R
+import com.m2mmusic.android.databinding.ActivitySplashBinding
+import kotlinx.coroutines.*
 
+/**
+ * 闪屏页
+ */
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        // ViewBinding加载布局
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashText.text = "M2M Music"
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            delay(2000)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
