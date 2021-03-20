e0d6b6e565028759f89f9fe1eb6e59f898847954
gsw <guosuweilw@qq.com>
2021-03-20 14:52:03 +0800

    Renew somethings.

--- app/src/main/java/com/m2mmusic/android/ui/MainActivity.kt
+++ app/src/main/java/com/m2mmusic/android/ui/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.m2mmusic.android
+package com.m2mmusic.android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.m2mmusic.android.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
