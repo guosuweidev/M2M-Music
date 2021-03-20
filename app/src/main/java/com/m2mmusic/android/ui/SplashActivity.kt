03fdb5214545150014100f056d79fd7f1ed96b0a
gsw <guosuweilw@qq.com>
2021-03-20 16:07:58 +0800

    chore: 增加闪屏页、日志工具类和全局Context获取

---
+++ app/src/main/java/com/m2mmusic/android/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.m2mmusic.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.m2mmusic.android.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
